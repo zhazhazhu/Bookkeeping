173789ddfbffd6b5e1132ca8f8f8e547648a3391
zhazhazhu <fangyuan.leslie@qq.com>
2021-05-26 19:53:25 +0800

    创建了三个页面

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <hr />
+    <router-view />
+    <router-link :to="{
+      path:'/labels'
+    }">标签</router-link>
+    <router-link :to="{
+      path:'/money'
+    }">记账</router-link>
+    <router-link :to="{
+      path:'/statistics'
+    }">统计</router-link>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
---
+++ src/views/Labels.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    labels.vue
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    
+  })
+</script>
+
+<style scoped>
+
+</style>

e82f4615aff87db3be68068adceb9b3bfba82e79
zhazhazhu <fangyuan.leslie@qq.com>
2021-06-02 20:28:15 +0800

    :sparkles: 新增统计页组件

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,15 +1,21 @@
 <template>
   <Layout>
-    Statistics.vue
+    <Types :value.sync="value"></Types>
   </Layout>
 </template>
 
 <script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Types from '@/components/Money/Types.vue';
 
-export default({
-  name: "Statistics",
-});
+@Component({
+  components: { Types },
+})
+export default class Statistics extends Vue {
+  value="-"
+}
 </script>
 
-<style scoped>
-</style>
+<style lang="scss" scoped>
+</style> scoped>

ac7a8bbcf57d0b5a713233a9497188cdee531f2f
zhazhazhu <fangyuan.leslie@qq.com>
2021-06-06 15:38:19 +0800

    dist

---
+++ precache-manifest.861b1eb64baf830ec00ad1041ae7624f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ead698eede888265e5e",
+    "url": "/css/app.53994e35.css"
+  },
+  {
+    "revision": "1e08cce6faa8fecc9ebfb5ebe232006d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ead698eede888265e5e",
+    "url": "/js/app.ac2266d3.js"
+  },
+  {
+    "revision": "e33a6034bc337525c02c",
+    "url": "/js/chunk-vendors.492eabdf.js"
+  },
+  {
+    "revision": "d1b00465103b8de3edb45d0584a5e5c4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
